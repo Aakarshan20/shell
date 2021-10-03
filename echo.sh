94ebe41a80642cef6dd835eb253ba5ab5d1cb04c
root <root@vagrant.vm>
2021-10-03 15:59:14 +0000

    shell 基本語法測試

---
+++ echo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Author: abc
+#Script Description: first shell study script
+echo "test";
+#expr 3 \* 3
+
+echo -n "Date is: "; date +%F
+
+echo "Date is: "; date +%F
+
+echo "Date is: `date +%F`"
